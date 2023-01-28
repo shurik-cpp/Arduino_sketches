f29ea14d5b5f6f00c26828d648e4be5295261a81
shurik-cpp <shurik.cpp@gmail.com>
2023-01-28 16:48:35 +0700

    Небольшие оптимизации

--- esp8266web/esp8266web.ino
+++ esp8266web/esp8266web.ino
@@ -55,7 +55,7 @@ String LedStatus() {                                                 // Функ
 	return String(!digitalRead(led));                                                 // возвращаем результат, преобразовав число в строку
 }
 
-bool handleFileRead(String path) {                                       // Функция работы с файловой системой
+bool handleFileRead(const String& path) {                                       // Функция работы с файловой системой
 	if (path.endsWith("/")) 											// Если устройство вызывается по корневому адресу, то должен вызываться файл index.html (добавляем его в конец адреса)
 		path += "index.html";                          
 	String contentType = getContentType(path);                            // С помощью функции GetContentType (описана ниже) определяем по типу файла (в адресе обращения) какой заголовок необходимо возвращать по его вызову
@@ -69,7 +69,7 @@ bool handleFileRead(String path) {                                       // Фу
 	return false;                                                         // Завершаем выполнение функции, возвращая результатом ее исполнения false (если не обработалось предыдущее условие)
 }
 
-String getContentType(String filename){                                 // Функция, возвращающая необходимый заголовок типа содержимого в зависимости от расширения файла
+String getContentType(const String& filename){                                 // Функция, возвращающая необходимый заголовок типа содержимого в зависимости от расширения файла
 	if (filename.endsWith(".html")) return "text/html";                   // Если файл заканчивается на ".html", то возвращаем заголовок "text/html" и завершаем выполнение функции
 	else if (filename.endsWith(".css")) return "text/css";                // Если файл заканчивается на ".css", то возвращаем заголовок "text/css" и завершаем выполнение функции
 	else if (filename.endsWith(".js")) return "application/javascript";   // Если файл заканчивается на ".js", то возвращаем заголовок "application/javascript" и завершаем выполнение функции
